0d71436a3f845da9a757b36456626c7a4aaf3d98
Ruth <rtilahun@brynmawr.edu>
2025-02-21 15:50:28 -0500

    organization

--- scripts/processing_scripts/pennaeps/data_processing.py
+++ scripts/processing_scripts/pennaeps/data_processing.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import numpy as np
+import os
 
+processed_csv = "processed_data.csv"
+# load existing data if it exists
+if os.path.exists(processed_csv):
+    existing_data = pd.read_csv(processed_csv)
+else:
+    existing_data = pd.DataFrame()
+
+# process the new data into a DataFrame, skippin ghte fisrt two rows
 file_path = '../../../data/pennaeps/Qualified_Facilities_Report.csv'
-
-# load the data into a DataFrame, skippin ghte fisrt two rows
 df = pd.read_csv(file_path, skiprows=2)
 
 # display the first few rows of the DataFrame
@@ -27,3 +34,14 @@ print('Column labels: ')
 print(df.columns)
 
 print('---------------------------------------------------')
+
+#get unique values in the 'State' column 
+print('Unique values in the State column: ')
+print(df['State'].unique())
+print('---------------------------------------------------')
+
+print('Drop all sataes except PA')
+df = df[df['State'] == 'PA']
+print(df['State'].unique())
+
+
